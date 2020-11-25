8f462a75d1576ec8409e10cc26d9edd436b30209
umezaki <1701001@st.asojuku.ac.jp>
2020-11-25 10:12:47 +0900

    update two-factor-authentication-setting

--- router/two-factor-authentication-setting.js
+++ router/two-factor-authentication-setting.js
@@ -3,6 +3,7 @@ const router = new Router();
 const connection = require('../app/db');
 const app = require('../app/app');
 const parser = require('ua-parser-js');
+const dateformat = require('dateformat');
 
 router.get('/two-factor-authentication-setting', async (ctx) => {
     let session = ctx.session;
@@ -23,19 +24,69 @@ router.get('/two-factor-authentication-setting', async (ctx) => {
 
     let sql = 'SELECT pc_uuid, env_ua, env_ip, validity_flag, timestamp FROM user_login_pc WHERE user_id = ?';
     let [data] = await connection.query(sql, [userId]);
-    console.log(data);
 
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            console.log(parser(data[key]['env_ua']))
+    result['data']['login_pc'] = data.map(value => {
+        let ua = parser(value['env_ua']);
+
+        let uaBrowser = ua.browser.name === undefined ? '不明なブラウザ' : `${ua.browser.name} ${ua.browser.major}`;
+        let uaOs = ua.os.name === undefined ? '不明なOS' : `${ua.os.name} ${ua.os.version}`;
+
+        return {
+            uuid: value['pc_uuid'],
+            env_name: `${uaBrowser}（${uaOs}）`,
+            env_ua: value['env_ua'],
+            env_ip: value['env_ip'],
+            validity_flag: value['validity_flag'],
+            timestamp: dateformat(value['timestamp'], 'yyyy年mm月dd日 HH時MM分ss秒')
         }
+    });
+
+    if (session.success !== undefined) {
+        result['data']['success'] = session.success;
+        session.success = undefined;
     }
 
-    // if (ua.browser.name === undefined || !['chrome', 'firefox'].includes(ua.browser.name.toLowerCase())) {
-    //     result['data']['meta']['browser_warning'] = true;
-    // }
+    if (session.error !== undefined) {
+        result['data']['error'] = session.error;
+        session.error = undefined;
+    }
 
     await ctx.render('two-factor-authentication-setting', result);
+});
+
+router.post('/two-factor-authentication-setting', async (ctx) => {
+    let session = ctx.session;
+    app.initializeSession(session);
+
+    let authId = session.auth_id;
+    let userId = await app.getUserId(authId);
+    if (!userId) {
+        session.error.message = 'ログインしていないため続行できませんでした';
+
+        return ctx.redirect('/login');
+    }
+
+    let uuid = ctx.request.body['uuid'];
+    if (uuid === undefined || uuid === '') {
+        session.error.message = 'データが見つかりませんでした';
+
+        return ctx.redirect('/two-factor-authentication-setting');
+    }
+
+    let sql = 'SELECT * FROM user_login_pc WHERE pc_uuid = ? AND user_id = ?';
+    let [data] = await connection.query(sql, [uuid, userId]);
+    if (data.length === 0) {
+        session.error.message = 'データが見つかりませんでした';
+
+        return ctx.redirect('/two-factor-authentication-setting');
+    }
+
+    sql = 'DELETE FROM user_login_pc WHERE pc_uuid = ?';
+    await connection.query(sql, [uuid]);
+
+    session.success.message = 'パソコン情報を削除しました';
+
+    ctx.redirect('/two-factor-authentication-setting');
 });
 
 module.exports = router;
